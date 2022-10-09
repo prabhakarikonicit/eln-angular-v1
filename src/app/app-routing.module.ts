d38f40be36725fb3749e21e553b7baeedd0baea8
prabhakarikonicit <109589313+prabhakarikonicit@users.noreply.github.com>
2022-10-09 22:46:10 +0530

    default code base
    default code base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagesBlankComponent } from './pages/pages-blank/pages-blank.component';
+import { PagesContactComponent } from './pages/pages-contact/pages-contact.component';
+import { PagesError404Component } from './pages/pages-error404/pages-error404.component';
+import { PagesFaqComponent } from './pages/pages-faq/pages-faq.component';
+import { PagesLoginComponent } from './pages/pages-login/pages-login.component';
+import { PagesRegisterComponent } from './pages/pages-register/pages-register.component';
+import { UsersProfileComponent } from './pages/users-profile/users-profile.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pages-blank', component: PagesBlankComponent },
+  { path: 'pages-contact', component: PagesContactComponent },
+  { path: 'pages-error404', component: PagesError404Component },
+  { path: 'pages-faq', component: PagesFaqComponent },
+  { path: 'pages-login', component: PagesLoginComponent },
+  { path: 'pages-register', component: PagesRegisterComponent },
+  { path: 'user-profile', component: UsersProfileComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
