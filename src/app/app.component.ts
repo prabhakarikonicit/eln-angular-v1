2608ab1c70c3fd52df018d6f9cbc1848c6d4f24a
prabhakarikonicit <koppireddyprabhakar@ikonicit.com>
2022-10-09 22:07:43 +0530

    initial commit
    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component ,ElementRef} from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'eln-angular-v1';
+  
+  constructor(private elementRef: ElementRef,  public  _router: Router) { }
+
+  ngOnInit() {
+
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "../assets/js/main.js";
+    this.elementRef.nativeElement.appendChild(s);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsersProfileComponent } from './pages/users-profile/users-profile.component';
+import { PagesFaqComponent } from './pages/pages-faq/pages-faq.component';
+import { PagesContactComponent } from './pages/pages-contact/pages-contact.component';
+import { PagesRegisterComponent } from './pages/pages-register/pages-register.component';
+import { PagesLoginComponent } from './pages/pages-login/pages-login.component';
+import { PagesError404Component } from './pages/pages-error404/pages-error404.component';
+import { PagesBlankComponent } from './pages/pages-blank/pages-blank.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    UsersProfileComponent,
+    PagesFaqComponent,
+    PagesContactComponent,
+    PagesRegisterComponent,
+    PagesLoginComponent,
+    PagesError404Component,
+    PagesBlankComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
